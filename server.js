bd996da9ac86fc6be56821017262682af79659de
LogyApp <seguimientologyser@gmail.com>
2025-11-29 10:55:06 -0500

    Release v.29.11.2025.1054 | Limit server upgrade

--- server.js
+++ server.js
@@ -17,8 +17,9 @@ import { fileURLToPath } from "url";
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(express.Router())
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 import correoAspiranteRoutes from "./router/correoAspirante.js";
 
